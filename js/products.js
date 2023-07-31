48294bc804b0126198f98f2bc6d5bbb735b2b117
alejandroramirez93 <140989948+alejandroramirez93@users.noreply.github.com>
2023-07-30 21:37:46 -0500

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Vino",
+        precio: "100",
+        img:
+            "https://tiendasyaco.vtexassets.com/arquivos/ids/155757-800-450?v=637535184875570000&width=800&height=450&aspect=true",
+    },
+    {
+        id: 2,
+        nombre: "Cerveza",
+        precio: "20",
+        img:
+        "https://cdn.inoutdelivery.com/beerstation.inoutdelivery.com/lg/1591913755845-5313-CERVEZA%20HEINEKEN%20BEER%20STATION.png",
+    },
+    {
+        id: 3,
+        nombre: "Vodka",
+        precio: "150",
+        img:
+        "https://mercaldas.vtexassets.com/arquivos/ids/1290205-800-auto?v=637958198212470000&width=800&height=auto&aspect=true",
+    },
+    {
+        id: 4,
+        nombre: "Agua",
+        precio: "10",
+        img:
+        "https://www.lenntech.com/images/Water/bottled.png",
+    },
+    {
+        id: 5,
+        nombre: "Ron",
+        precio: "90",
+        img: 
+        "https://bevgo.com.co/wp-content/uploads/2021/05/82041.jpg",
+    },
+];
